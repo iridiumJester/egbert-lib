f4d7c230c0e85d2d04a99f7ea2c6f18af7fb7bf6
iridiumJester <145147938+iridiumJester@users.noreply.github.com>
2024-03-19 14:53:14 -0700

    Dont Mind Me Just Making The Skeleton Of This Thing

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Egbert Lib
+by iridiumJester
+Mad Lib-like project.
+"""
+
+#import statements
+
+
+#welcome user
+
+
+#get user input
+
+
+#build story
+
+
+#show results
+
+
+#send the user your fondest regards and quit

f6abefb8cb6d91a9bcdd7fd66b062c553572485d
iridiumJester <145147938+iridiumJester@users.noreply.github.com>
2024-03-19 15:29:53 -0700

    User Inputs

--- project/app.py
+++ project/app.py
@@ -5,7 +5,7 @@ Mad Lib-like project.
 """
 
 #import statements
-
+#wait. what does that mean
 
 #welcome user
 print("Greetings, user. Would you like to play a game? A word game?")
@@ -13,11 +13,26 @@ play = input("Seriously. Yes/No: ")
 if play == "yes":
     print("How wonderful!")
 else:
-    print("Huh? Alright then, close the tab.")
-    final_confirm = input("Change your mind? Press enter to continue program.")
+    confirm = input("Huh? Alright then. Change your mind? Press enter to continue.")
 
 #get user input
-
+adj1 = input("Please enter an adjective: ")
+noun1 = input("A noun: ")
+pronoun_obj = input("An objective pronoun (his, hers, their): ")
+pronoun_sub = input("A subjective pronoun (he, she, they): ")
+date = input("A date (Ex: 7th of July, 2007): ")
+num = int(input("A number below 100: "))
+name = input("A first name: ")
+plural_noun1 = input("A plural noun: ")
+adj2 = input("An adjective: ")
+plural_noun2 = input("Another plural noun: ")
+verb1 = input("A verb NOT ending in -ing: ")
+plural_noun3 = input("Another another plural noun: ")
+noun2 = input("A noun: ")
+adj3 = input("An adjective: ")
+noun3 = input("A noun: ")
+verb2 = input("A verb: ")
+plural_noun4 = input("One last plural noun: ")
 
 #build story
 

57ae0b902ef3744d3c703bbe4950799f579493cc
iridiumJester <145147938+iridiumJester@users.noreply.github.com>
2024-03-19 15:14:03 -0700

    Oh Hey Just More Setup Things Carry On

---
+++ project/app.py
@@ -0,0 +1,28 @@
+"""
+Egbert Lib
+by iridiumJester
+Mad Lib-like project.
+"""
+
+#import statements
+
+
+#welcome user
+print("Greetings, user. Would you like to play a game? A word game?")
+play = input("Seriously. Yes/No: ")
+if play == "yes":
+    print("How wonderful!")
+else:
+    print("Huh? Alright then, close the tab.")
+    final_confirm = input("Change your mind? Press enter to continue program.")
+
+#get user input
+
+
+#build story
+
+
+#show results
+
+
+#send the user your fondest regards and quit
